4a6b739e7fe439933c3418301e34e89d7acf2f18
clerici03 <115361946+clerici03@users.noreply.github.com>
2022-12-07 09:31:18 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
-
+#include "let.h"
+bool funzione (char &a){
+    if ((a>=65 & a<=90) or (a>=97 & a<=122)){
+        if (a>= 65 & a<=95){
+            a+=32;
+        }else {
+            a-=32;
+        }
+        return true;
+    }else {
+        return false;
+    }
+}
